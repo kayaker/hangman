9da031df42747e0f6c18e0850be321e73f04b031
Daniel Spurgeon <danielspurgeon@gmail.com>
2014-05-12 21:01:33 -0500

    initial project version

---
+++ hangman.py
@@ -0,0 +1,21 @@
+#hangman
+
+wordtoguess = list("apple")
+
+hashed_word = ""
+guess_a_letter = "p"
+x = 0
+x2=x
+letter_2_guess = raw_input("Guess a letter: ")
+x = wordtoguess.count(letter_2_guess)
+print "That letter was found: " + str(x) + " times!"
+p = 0
+
+while hashed_word != wordtoguess:
+    for f in wordtoguess:
+        if f == letter_2_guess:
+            hashed_word = "x" * 5
+            print hashed_word
+        else:
+            hashed_word = hashed_word + "x"
+    letter_2_guess = raw_input("Guess a letter: ")
